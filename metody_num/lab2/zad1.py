21a4a6144d661f5661718123a9db51b43b4d5e85
fefeo <glinkowski@onet.eu>
2019-03-17 16:32:39 +0100

    done task1

--- metody_num/lab2/zad1.py
+++ metody_num/lab2/zad1.py
@@ -2,5 +2,20 @@ from matplotlib.pyplot import *
 from numpy import *
 from cs50 import get_int , get_float
 
-print('If radius is float, press 'f': ')
-print('If radius is int, press 'i': ')
+print('FOR X')
+print('If the radius is float, press "f" ')
+print('If the radius is int, press "i" ')
+print('_'*20)
+z=str(input('"f" or "i"?   '))
+
+
+if z== 'f':
+    X=get_float("put int radius X: ")
+    F=pi*(X)**2
+    P=2*pi*X
+    print("field: ",F,"parimeter: ",P)
+elif z == 'i':
+    X = get_int("put float radius X: ")
+    F = pi * (X) ** 2
+    P = 2 * pi * X
+    print("field: ", F, "parimeter: ", P)
