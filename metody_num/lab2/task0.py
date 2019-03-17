45c4a80c04d00efd09b1f54afed6d5f1afd1ba60
fefeo <glinkowski@onet.eu>
2019-03-17 15:48:25 +0100

    typing task 1

---
+++ metody_num/lab2/task0.py
@@ -0,0 +1,5 @@
+from cs50 import get_int , get_float
+
+
+x = get_int("put int x value: ")
+
---
+++ metody_num/lab2/zad1.py
@@ -0,0 +1,6 @@
+from matplotlib.pyplot import *
+from numpy import *
+from cs50 import get_int , get_float
+
+print('If radius is float, press 'f': ')
+print('If radius is int, press 'i': ')
