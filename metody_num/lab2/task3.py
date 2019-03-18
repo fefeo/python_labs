dcb64666c28e8cc76ea840328f8335a21adef0cf
fefeo <glinkowski@onet.eu>
2019-03-18 22:04:01 +0100

    task 3 updated

--- metody_num/lab2/task3.py
+++ metody_num/lab2/task3.py
@@ -3,5 +3,7 @@ from cs50 import get_int , get_float
 x=get_int('x: ')
 y=get_int('y: ')
 
-ifdivisible= 'X is divisible by Y' if x & y == 0 else 'X is not divisible by Y'
+ifdivisible= 'X is divisible by Y' if y != 0 and x !=0 and x % y == 0  else 'X is not divisible by Y'
 print(ifdivisible)
+
+print
