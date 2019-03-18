3f885e478131415c394a6346da54b3f0d5386ada
fefeo <glinkowski@onet.eu>
2019-03-18 21:49:10 +0100

    task 2 updated

--- metody_num/lab2/task2.py
+++ metody_num/lab2/task2.py
@@ -1,9 +1,20 @@
 from numpy import *
-s=int(input('set range from: '))
-e=int(input('set range to: '))
-for x in range(s,e):
-    for y in range(s,e):
-        if x%y ==0:
-            if x % 2 == 0 and y % 2 == 0:
-                print("x: ",x,"y: ",y)
+from cs50 import get_int
+
+s = get_int('set range from: ')
+e = get_int('set range to: ')
+if s >= 0 and e >= 0:
+    for x in range(s , e):
+        for y in range(s , e):
+            if x%y ==0:
+                if x % 2 == 0 and y % 2 == 0:
+                    print("x: ",x,"y: ",y)
+elif s == 0 or e == 0:
+    for x in range(s+1,e+1):
+        for y in range(s+1,e+1):
+            if x%y ==0:
+                if x % 2 == 0 and y % 2 == 0:
+                    print("x: ",x,"y: ",y)
+else:
+    print("wrong range")
 
