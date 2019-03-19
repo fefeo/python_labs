a0af15fa349da0dce5c8d2db6baa2e0d6eba49db
fefeo <glinkowski@onet.eu>
2019-03-19 09:36:39 +0100

    cuted and extra protection in task1

--- metody_num/lab2/task1.py
+++ metody_num/lab2/task1.py
@@ -1,39 +1,14 @@
-from matplotlib.pyplot import *
 from numpy import *
 from cs50 import get_int , get_float
 
-print('FOR X')
-print('If the radius is float, press "f" ')
-print('If the radius is int, press "i" ')
-print('_'*20)
-z=str(input('"f" or "i"?   '))
+x = get_float("put radius x: ")
+y = get_float("put radius y: ")
 
+while x <= 0 or y <= 0:
+    x = get_float("put radius x: ")
+    y = get_float("put radius y: ")
+for i in [x,y]:
+    F = pi * (i**2)
+    P = 2 * pi * i
+    print('for radius', i ,'Field = ', F ,'\n', 'Parimeter = ', P)
 
-if z== 'f':
-    X=get_float("put float radius X: ")
-    F=pi*(X)**2
-    P=2*pi*X
-    print("field: ",F,"parimeter: ",P)
-elif z == 'i':
-    X = get_int("put int radius X: ")
-    F = pi * (X) ** 2
-    P = 2 * pi * X
-    print("field: ", F, "parimeter: ", P)
-
-print('FOR Y')
-print('If the radius is float, press "f" ')
-print('If the radius is int, press "i" ')
-print('_'*20)
-z=str(input('"f" or "i"?   '))
-
-
-if z== 'f':
-    Y=get_float("put float radius Y: ")
-    F=pi*(Y)**2
-    P=2*pi*Y
-    print("field: ",F,"parimeter: ",P)
-elif z == 'i':
-    Y = get_int("put int radius Y: ")
-    F = pi * (Y) ** 2
-    P = 2 * pi * Y
-    print("field: ", F, "parimeter: ", P)
